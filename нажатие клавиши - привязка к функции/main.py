0de078b12cae3f2f803e1f626f270e1a45434a55
sazanov-ilya <sazanov.ilya.85@gmail.com>
2021-07-31 17:53:28 +0300

    20210731_1800

---
+++ нажатие клавиши - привязка к функции/main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+
+class app(tk.Frame):
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("300x200")
+        self.label = tk.Label(self.root, text="")
+        self.label.pack()
+        self.root.bind('<Return>', self.callback)
+        self.root.mainloop()
+
+    def callback(self, event):
+
+        # Мы поместили атрибут keysym объекта event в показанную метку.
+        # keysym является ключевым символом события клавиатуры. Enter - это Return, как мы ввели выше.
+        self.label["text"] = "You pressed {}".format(event.keysym)
+
+
+app()
